ffe6ee9beb0a5e3945921068ecd146ad7c28f1b5
David <davidporter33@icloud.com>
2018-08-27 13:05:08 -0700

    First commit, learning and practicing TDD in Swift

--- BowlingGameTests/BowlingGameTests.swift
+++ BowlingGameTests/BowlingGameTests.swift
@@ -11,5 +11,34 @@ import XCTest
 
 class BowlingGameTests: XCTestCase {
 
+    var game: Game!
+
+    override func setUp() {
+        super.setUp()
+        game = Game()
+    }
+
+    override func tearDown() {
+        game = nil
+        super.tearDown()
+    }
+
+    func testGutterGame() {
+        let n = 20
+        let pins = 0
+        for _ in 1...n {
+            game.roll(pins)
+        }
+
+        XCTAssertEqual(game.score(), 0)
+    }
+
+    func testAllOnes() {
+        for _ in 1...20 {
+            game.roll(1)
+        }
+
+        XCTAssertEqual(game.score(), 20)
+    }
     
 }
